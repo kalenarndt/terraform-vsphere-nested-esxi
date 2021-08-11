c2dcb0e5597b7a23c75003e58e11ea017e4fdb84
kalenarndt <kalenarndt@gmail.com>
2021-08-11 21:05:34 +0000

    update readme

--- examples/multiple-host-deployment/main.tf
+++ examples/multiple-host-deployment/main.tf
@@ -1,14 +1,14 @@
 locals {
   # Deployment Details
-  infra_deploy   = yamldecode(file("${path.module}/deploy_hosts.yaml"))
+  infra_deploy = yamldecode(file("${path.module}/deploy_hosts.yaml"))
 
   # Nested Hosts
   parent_infra_details = local.infra_deploy.infra_config
   nested_host_config   = local.infra_deploy.nested_hosts
 }
 
-module "deploy_nested_hosts" {
-  source             = "github.com/kalenarndt/terraform-vsphere-nested-esxi"
+module "nested-esxi" {
+  source             = "kalenarndt/nested-esxi/vsphere"
   for_each           = local.parent_infra_details
   vsphere_datacenter = each.value.vsphere_datacenter
   vsphere_datastore  = each.value.vsphere_datastore
--- examples/multiple-host-deployment/terraform.tfvars
+++ examples/multiple-host-deployment/terraform.tfvars
@@ -1,3 +1,3 @@
-vcenter_server = "myvc.mydomain.com"
+vcenter_server   = "myvc.mydomain.com"
 vcenter_username = "administrator@vsphere.local"
 vcenter_password = "VMware1!"
